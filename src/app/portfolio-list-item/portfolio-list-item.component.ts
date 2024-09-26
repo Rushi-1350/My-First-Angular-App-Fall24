7ace93840174375d602b00d6d10fa9b6c32e8d4a
Rushi-1350 <w0847987@myscc.ca>
2024-09-26 15:17:15 -0400

    Created PortfolioList and PortfolioListItem components for displaying content

---
+++ src/app/portfolio-list-item/portfolio-list-item.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-portfolio-list-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './portfolio-list-item.component.html',
+  styleUrl: './portfolio-list-item.component.css'
+})
+export class PortfolioListItemComponent {
+
+}
