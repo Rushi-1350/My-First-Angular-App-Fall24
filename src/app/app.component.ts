90b31aa2b0d7e0920fec930505dece85dc05cd37
Rushi Patel <w0847987@myscc.ca>
2024-10-10 16:33:15 -0400

    added a single ContentListItem item, and  new service to the AppComponent, using dependency injection.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { PortfolioListComponent } from './portfolio-list/portfolio-list.component';  
+import { IContent } from './models/content.model';
+import { PortfolioService } from './Services/portfolio.service';
+import { IContent } from './models/content.model';
 // Imported PortfolioListComponent
 
 @Component({
@@ -11,7 +14,17 @@ import { PortfolioListComponent } from './portfolio-list/portfolio-list.componen
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  
-}
+export class AppComponent implements OnInit{
+  title = 'Projects';
 
+  // definfing a single item 
+
+  item: IContent = {
+    title: 'Carousel',
+    description: 'This is a carousel project',
+    technologies: ['JavaScript', 'HTML', 'CSS'],
+    link: '#',
+    completed: true
+  };
+
+}
